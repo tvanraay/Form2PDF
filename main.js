8be1dd38ed910914ff15f623ff5e2cd5a7a1876e
Usman Zaheer <usmanozaheer@gmail.com>
2023-03-02 22:30:09 -0500

    init

--- main.js
+++ main.js
@@ -13,26 +13,3 @@ app.whenReady().then(() => {
     createWindow()
 })
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
-
-
-  app.whenReady().then(() => {
-    createWindow()
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
-
-  window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-  
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency])
-    }
-  })
